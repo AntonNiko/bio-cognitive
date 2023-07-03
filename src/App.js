8e97cc16b7bcb76bc5e9151472e00db8baefcdd3
Anton Nikitenko <ant.nikitenko@gmail.com>
2023-07-02 20:26:20 -0700

    Minor changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+let guest = 0;
+
+function Form() {
+  return (
+    <form onSubmit={(e) => {
+      e.preventDefault();
+    }}>
+      <input type='text'/>
+      <button>Hello!</button>
+    </form>
+  )
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Form/>
   );
 }
+
 
 export default App;
